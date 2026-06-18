1b47d3d02319c2304ed8abb439164bf46c7b767e
agustinDuoc <agu.perez@duocuc.cl>
2026-06-18 19:27:15 -0400

    Quitar archivos duplicados del proyecto

--- ms-ordenes-compra/.mvn/src/main/java/com/supplyhub/ms_ordenes_compra/exception/RecursoNoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package com.supplyhub.ms_ordenes_compra.exception;
-
-public class RecursoNoEncontradoException extends RuntimeException {
-
-    public RecursoNoEncontradoException(String mensaje) {
-        super(mensaje);
-    }
-}
